8aa82b1693cb4fa640e703c8031ad6206751d9b0
Aritra Ghosh <aritraghsh09@gmail.com>
2016-11-11 14:52:10 +0100

    Update Install.sh
    Adding support for non-existence of bin , share and other subfolders

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+if [ ! -d "~/.local/bin" ]; then
+  mkdir ~/.local/bin
+fi
 mkdir ~/.local/bin/comEt_Me/
 gcc -o comEt_Me comEt_Me.c -lm -lncurses
 cp comEt_Me Menu.h ~/.local/bin/comEt_Me/
+if [ ! -d "~/.local/share" ]; then
+  mkdir ~/.local/share
+fi
+if [ ! -d "~/.local/share/applications" ]; then
+  mkdir ~/.local/share/applications
+fi
+if [ ! -d "~/.local/share/icons" ]; then
+  mkdir ~/.local/share/icons
+fi
 cp comEt_Me.desktop ~/.local/share/applications/
 cp comEt_Me.png ~/.local/share/icons/
+as
