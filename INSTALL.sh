93d34961a02dc4aa190a24ad36fa2590a08baa18
Arkoprovo1996 <arkoprovo1996@gmail.com>
2016-11-12 23:34:46 +0530

    Added to make the .desktop work

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-if [ ! -d "~/.local/bin" ]; then
+if [ ! -d ~/.local/bin/comEt_Me/ ]; then
   mkdir ~/.local/bin
 fi
-mkdir ~/.local/bin/comEt_Me/
+
+if [ ! -d ~/.local/bin/comEt_Me/ ]; then
+  mkdir ~/.local/bin/comEt_Me/
+fi
+
 gcc -o comEt_Me comEt_Me.c -lm -lncurses
+
 cp comEt_Me Menu.h ~/.local/bin/comEt_Me/
-if [ ! -d "~/.local/share" ]; then
+
+if [ ! -d ~/.local/share ]; then
   mkdir ~/.local/share
 fi
-if [ ! -d "~/.local/share/applications" ]; then
+
+if [ ! -d ~/.local/share/applications ]; then
   mkdir ~/.local/share/applications
 fi
-if [ ! -d "~/.local/share/icons" ]; then
+
+if [ ! -d ~/.local/share/icons ]; then
   mkdir ~/.local/share/icons
 fi
+
 cp comEt_Me.desktop ~/.local/share/applications/
 cp comEt_Me.png ~/.local/share/icons/
+
+if [[ ! ":$PATH:" == *":$HOME/.local/bin/comEt_Me/:"* ]]; then
+  echo export PATH=$PATH:~/.local/bin/comEt_Me/: >> ~/.bashrc
+fi
+
+. ~/.bashrc
